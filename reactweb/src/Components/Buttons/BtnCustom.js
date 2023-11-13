73b367bd0e1c2165c9918d65c30d25d4c3d92a40
Andrei <barbutodev@gmail.com>
2023-11-13 15:58:17 -0300

    Tela Home apenas front

---
+++ reactweb/src/Components/Buttons/BtnCustom.js
@@ -0,0 +1,10 @@
+import style from './BtnCustom.css';
+
+const BtnCustom = ({label, customStyle, onClick}) =>{
+
+    return(
+        <button onClick={onClick} className="btnCustom"  style={customStyle}>{label}</button>
+    )
+}
+
+export default BtnCustom;
--- reactweb/src/Pages/Home/Home.js
+++ reactweb/src/Pages/Home/Home.js
@@ -1,9 +1,47 @@
+import BtnCustom from '../../Components/Buttons/BtnCustom';
 import '../Home/home.style.css';
 
-const Home = () =>{
-    return(
-        <div >
-            <h1>Esta é a tela Home</h1>
+const Home = () => {
+    return (
+        <div className='container-home'>
+            <div className="wrapper-home">
+                <div className='header-home'>
+                    <h1>Home</h1>
+                    <BtnCustom
+                                
+                                label={"LOGOUT"}/>
+
+                </div>
+                <div className='sub-container'>
+                    <div className='info-user'>
+                        <h1>Bem vindo</h1>
+                        <h2>Astolfo</h2>
+                    </div>
+                    <hr className='linha'></hr>
+                    <div className='container-estabelecimentos-user'>
+                        <h1> Seus estabelecimentos</h1>
+                        {/* implementar MAP */}
+                        <div className='estabelecimento'>
+                            <div className='nome-endereco'>
+                                <h1>Chimarrom</h1>
+                                <h2>Rua Rio Branco nº 1500</h2>
+                            </div>
+                            <div className='btn-editar'>
+                                <BtnCustom
+                                
+                                label={"EDITAR"}/>
+                            </div>
+                            
+                        </div>
+                        
+
+
+
+                    </div>
+                </div>
+
+
+            </div>
         </div>
     );
 }
